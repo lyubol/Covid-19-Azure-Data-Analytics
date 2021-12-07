e546ec972a24b64131d70d3570a00db46671caf7
Lyubomir Lirkov <L_L@adatis.co.uk>
2021-12-07 11:08:32 +0200

    Added Stage schema and tables

---
+++ Stage/Tables/Stage.Cases.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Stage].[Cases]
+(
+	[Key] VARCHAR(14) NOT NULL PRIMARY KEY,
+	[Province/State] VARCHAR(50) NULL,
+	[Country/Region] VARCHAR(50) NULL,
+	[Latitude] DECIMAL(11,8) NULL,
+	[Longitude] DECIMAL(11,8) NULL,
+	[Date] DATE NULL,
+	[Value] INT NULL,
+	[CountryCode] CHAR(3) NULL,
+	[RegionCode] INT NULL,
+	[SubRegionCode] INT NULL,
+	[IntermediateRegionCode] INT NULL,
+	[LoadDate] DATETIME NOT NULL CONSTRAINT DF_Stage_Cases_LoadDate DEFAULT (SYSUTCDATETIME())
+)
