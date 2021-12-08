fa4e4816da5881b7d2a809bb901360328c94d420
Lyubomir Lirkov <L_L@adatis.co.uk>
2021-12-08 18:16:22 +0200

    1. Added 'Dim' schema; 2. Added table Dim.Countries; 3. Added Post-Deploymnet Script for Dim.Countries.

---
+++ Azure SQL Database/Dim/Tables/Dim.Country.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Dim].[Country]
+(
+	[CountryId] INT NOT NULL PRIMARY KEY,
+    [CountryCode] CHAR(3) NOT NULL, 
+    [Country/Region] VARCHAR(50) NOT NULL,
+    [RegionCode] INT NOT NULL,
+    [SubRegionCode] INT NOT NULL,
+    [IntermediateRegionCode] INT NOT NULL
+)
