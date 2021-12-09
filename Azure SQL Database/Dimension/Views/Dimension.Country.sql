b755070eabc240125ed9172edda6aa0c40d2f5a9
Lyubomir Lirkov <L_L@adatis.co.uk>
2021-12-09 16:51:52 +0200

    Updated Dimension.CountryCode

--- Azure SQL Database/Dimension/Views/Dimension.Country.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [Dimension].[CountryCode]
-(
-	[CountryId] INT IDENTITY(1,1) PRIMARY KEY,
-    [CountryCode] CHAR(3) NOT NULL, 
-    [Country/Region] VARCHAR(50) NOT NULL,
-    [RegionCode] INT NOT NULL,
-    [SubRegionCode] INT NOT NULL,
-    [IntermediateRegionCode] INT NOT NULL
-)
