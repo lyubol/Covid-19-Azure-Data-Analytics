ee1e9b49bcbfd948b505cd572b71a1179b844556
Lyubomir Lirkov <L_L@adatis.co.uk>
2021-12-10 17:33:10 +0200

    Updated Stage.CountryDetailed data types

--- Azure SQL Database/Stage/Tables/Stage.CountryDetailed.sql
+++ Azure SQL Database/Stage/Tables/Stage.CountryDetailed.sql
@@ -5,12 +5,12 @@
 	[Country] VARCHAR(50) NULL,
 	[CountryCode] CHAR(3) NULL,
 	[Population] INT NULL,
-	[PopulationDensity] DECIMAL(10, 4) NULL,
-	[MedianAge] DECIMAL(4, 2) NULL,
-	[GdpPerCapita] DECIMAL(12, 4) NULL,
+	[PopulationDensity] DECIMAL(8, 3) NULL,
+	[MedianAge] DECIMAL(3, 1) NULL,
+	[GdpPerCapita] DECIMAL(9, 3) NULL,
 	[HumanDevelopmentIndex] DECIMAL(4, 3) NULL,
 	[LifeExpectancy] DECIMAL(4, 2) NULL,
-	[Aged65Older] DECIMAL(4, 3) NULL,
+	[Aged65Older] DECIMAL(5, 3) NULL,
 	[Aged70Older] DECIMAL(5, 3) NULL,
 	[LoadDate] DATETIME NOT NULL CONSTRAINT DF_Stage_Cases_LoadDate DEFAULT (SYSUTCDATETIME())
 )
